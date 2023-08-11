20633fdc4e89f621c6f847721e44db28b5dfebcd
SDScandrettKint <samuel.scandrett@k-int.co.uk>
2023-08-11 01:49:55 +0100

    allow duplicates for output structure

--- _plugins/elasticSearch/index.js
+++ _plugins/elasticSearch/index.js
@@ -23,7 +23,7 @@ module.exports = function (eleventyConfig, options) {
     return JSON.stringify(obj, (key, value) => {
       if (typeof value === 'object' && value !== null) {
         // Duplicate reference found, discard key
-        if (cache.includes(value)) return;
+        // if (cache.includes(value)) return;
 
         // Store value in our collection
         cache.push(value);
